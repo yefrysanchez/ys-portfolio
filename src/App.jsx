71f4303365cc3d98453e8115779fcd1769640776
Yefry Sanchez <y25sanchez@gmail.com>
2023-01-03 16:31:37 -0500

    pages routes set up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
+import AboutPage from './pages/AboutPage'
+import ContactPage from './pages/ContactPage'
+import HomePage from './pages/HomePage'
+import NotFoundPage from './pages/NotFoundPage'
+import ProjectsPage from './pages/ProjectsPage'
+import SkillsPage from './pages/SkillsPage'
 
 function App() {
 
   return (
-    <div className="App">
+    <>
       <Navbar />
-    </div>
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path='/about' element={<AboutPage />} />
+        <Route path='/skills' element={<SkillsPage />} />
+        <Route path='/projects' element={<ProjectsPage />} />
+        <Route path='/contact' element={<ContactPage />} />
+        <Route path='/*' element={<NotFoundPage />} />
+      </Routes>
+    </>
   )
 }
 
---
+++ src/pages/SkillsPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SkillsPage = () => {
+  return (
+    <div>
+        <h1>Skills</h1>
+    </div>
+  )
+}
+
+export default SkillsPage
