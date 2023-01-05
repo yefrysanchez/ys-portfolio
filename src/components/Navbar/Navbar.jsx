27df6aa3cc105de8e76654811078d4f749355c6d
Yefry Sanchez <y25sanchez@gmail.com>
2023-01-05 11:03:18 -0500

    navlinks and navbar done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,29 +1,50 @@
+import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 const Navbar = () => {
   return (
     <nav className="nav">
       <ul className="nav-links">
-        <li className="links links-home">
+        <NavLink
+          to="/"
+          className={`links links-home ${({ isActive }) =>
+            isActive ? "home" : ""}`}
+        >
           <i className="fa-solid fa-house "></i>
           <span>Home</span>
-        </li>
-        <li className="links links-about">
+        </NavLink>
+        <NavLink
+          to="/about"
+          className={`links links-about ${({ isActive }) =>
+            isActive ? "active" : ""}`}
+        >
           <i className="fa-solid fa-user "></i>
           <span>About</span>
-        </li>
-        <li className="links links-skill">
+        </NavLink>
+        <NavLink
+          to="skills"
+          className={`links links-skill ${({ isActive }) =>
+            isActive ? "active" : ""}`}
+        >
           <i className="fa-solid fa-laptop-code"></i>
           <span>Skills</span>
-        </li>
-        <li className="links links-project">
+        </NavLink>
+        <NavLink
+          to="projects"
+          className={`links links-project ${({ isActive }) =>
+            isActive ? "active" : ""}`}
+        >
           <i className="fa-regular fa-folder-open"></i>
           <span>Projects</span>
-        </li>
-        <li className="links links-contact">
+        </NavLink>
+        <NavLink
+          to="contact"
+          className={`links links-contact ${({ isActive }) =>
+            isActive ? "active" : ""}`}
+        >
           <i className="fa-solid fa-envelope"></i>
           <span>Contact</span>
-        </li>
+        </NavLink>
       </ul>
     </nav>
   );
